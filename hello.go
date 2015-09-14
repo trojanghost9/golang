13c098d3e8435b9dd73a3fd5818305b0188a40fb
rxduran <rxduran@godaddy.com>
2015-09-14 13:26:26 -0700

    added test

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
+// this is where i name the package
 package main
 
+// this is where my imports are
 import "fmt"
 
+// this is me defining a function that prints out "hello world"
 func main() {
     fmt.Printf("hello, world\n")
 }
---
+++ test.go
@@ -0,0 +1,10 @@
+// this is my test package
+package main
+
+// this is where my imports are
+import "fmt"
+
+// this is my test function
+func main() {
+    fmt.Printf("this is just a print out\ngoing to need to research a bit more\n")
+}
